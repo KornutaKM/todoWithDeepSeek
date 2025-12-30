ce677369c3ea584c3be72bd95ec347bba66a3779
Kirill Kornuta <onemorekorn@gmail.com>
2025-12-30 18:09:09 +0500

    исправил проверку нейросети с дипсиком

--- feature_postgres/simple_connection/simple_connection.go
+++ feature_postgres/simple_connection/simple_connection.go
@@ -3,22 +3,29 @@ package simpleconnection
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func CheckConnection() {
-	ctx := context.Background()
-	connection, err := pgx.Connect(ctx, "postgres://koxypo:admin@192.168.1.224:5432/mydb")
-	if err != nil {
-		fmt.Println("Ошибка при подключении к БД:", err)
-		panic(err)
-	}
+func CheckConnection() error {
+	// Используем контекст с таймаутом для предотвращения зависания
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	if err := connection.Ping(ctx); err != nil {
-		fmt.Println("Ошибка при подключении к БД:", err)
-		panic(err)
+	// Подключение к базе данных
+	connString := "postgres://koxypo:admin@192.168.1.224:5432/postgres"
+	conn, err := pgx.Connect(ctx, connString)
+	if err != nil {
+		return fmt.Errorf("не удалось подключиться к БД: %w", err)
+	}
+	defer conn.Close(ctx)
+
+	// Проверка соединения
+	if err := conn.Ping(ctx); err != nil {
+		return fmt.Errorf("не удалось проверить соединение с БД: %w", err)
 	}
 
 	fmt.Println("Подключение к базе данных успешно!")
+	return nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	simpleconnection "thirdApp/feature_postgres/simple_connection"
 	"thirdApp/handlers"
 )
 
 func main() {
-	simpleconnection.CheckConnection()
-	
+	if err := simpleconnection.CheckConnection(); err != nil {
+		log.Fatalf("Ошибка подключения: %v", err)
+	}
+
 	http.HandleFunc("/add", handlers.AddTaskHandler)
 	http.HandleFunc("/delete", handlers.DeleteTaskHandler)
 	http.HandleFunc("/getTask", handlers.GetTaskByID)
